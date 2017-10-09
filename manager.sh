3609c1b3bc854531bda44f339bc2c23b0458483d
rogovski <michaeljrogowski@gmail.com>
2017-10-09 17:48:24 -0400

    add start/stop for s3 and mongodb

--- manager.sh
+++ manager.sh
@@ -52,6 +52,50 @@ function start_bus {
   pm2 start docker --name kxp_bus -s -- run -i --rm --hostname bushost -p 5672:5672 --name bus rabbitmq:3
 }
 
+# dev database server (mongo)
+function stop_mongodb {
+  pm2 stop kxp_mongodb -s
+}
+# the tutum/mongodb container doesnt respond to sigint, so we have to
+# explicitly call docker stop on it. 
+# TODO: dig thru this docker file to learn more about it.
+function stop_mongodb_docker {
+  docker stop kxp_mongodb
+}
+function delete_mongodb {
+  pm2 delete kxp_mongodb -s
+}
+function start_mongodb {
+  pm2 start docker --name kxp_mongodb -s -- run -i --rm -p 27017:27017 -p 28017:28017 --name kxp_mongodb -e AUTH=no tutum/mongodb
+}
+function start_mongodb_lt {
+  docker run -d -p 27017:27017 -p 28017:28017 --name kxp_mongodb -e AUTH=no tutum/mongodb
+}
+
+# development blob (minio/s3)
+function stop_blob {
+  pm2 stop kxp_blob -s
+}
+function delete_blob {
+  pm2 delete kxp_blob -s
+}
+function start_blob {
+  pm2 start docker --name kxp_blob -s -- run \
+    -i --rm \
+    -e "MINIO_ACCESS_KEY=RX3ZLBBZ1N5O6447UZ4D" \
+    -e "MINIO_SECRET_KEY=9rAi04T4R3X0YKztCZTJdIkOjLdLrXw+unKLoIFE" \
+    -p 9000:9000 \
+    minio/minio server /export
+}
+function start_blob_lt {
+  docker run \
+    -d \
+    -e "MINIO_ACCESS_KEY=RX3ZLBBZ1N5O6447UZ4D" \
+    -e "MINIO_SECRET_KEY=9rAi04T4R3X0YKztCZTJdIkOjLdLrXw+unKLoIFE" \
+    -p 9000:9000 \
+    minio/minio server /export
+}
+
 # init webserver (copy bootstrap, run sass once)
 
 function start_devserver_init {
@@ -205,20 +249,31 @@ then
       exit
       ;;
     coreall)
+      start_blob
+      start_mongodb
       start_redis
       start_bus
+      echo "warming up cache, dal, and messaging infrastructure."
+      echo "please be patient."
+      echo "WARNING: sleep period is hardcoded in manager.sh"
+      sleep 25
       start_ctrlserver
-      start_devserver_init
+      start_devserver_init > /dev/null 2>&1
       DEVSRV_STAT=$(pm2 list | grep "kxp_devserver")
       if [ -z "$DEVSRV_STAT" ]
       then
-        start_devserver
+        start_devserver > /dev/null 2>&1
       fi
-      SASSSRV_STAT=$(pm2 list | grep "kxp_sasswatch")
-      if [ -z "$SASSSRV_STAT" ]
-      then
-        start_sasswatch
-      fi
+      # SASSSRV_STAT=$(pm2 list | grep "kxp_sasswatch")
+      # if [ -z "$SASSSRV_STAT" ]
+      # then
+      #   start_sasswatch
+      # fi
+      sleep 5
+      echo "initializing backend with development data."
+      # TODO: dev/prod switch needed.
+      # load_dev_env_data 
+      # tagged_ads
       pm2 list
       exit
       ;;
@@ -263,16 +318,21 @@ then
       exit
       ;;
     coreall)
+      stop_blob
+      stop_mongodb
+      stop_mongodb_docker
       stop_redis
       stop_bus
       stop_ctrlserver
       stop_devserver
-      stop_sasswatch
+      # stop_sasswatch
+      delete_blob
+      delete_mongodb
       delete_redis
       delete_bus
       delete_ctrlserver
       delete_devserver
-      delete_sasswatch
+      # delete_sasswatch
       pm2 list
       exit
       ;;
